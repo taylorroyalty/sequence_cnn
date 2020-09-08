718273e3206c5e7cc72aefc67e8a26997b78cf29
unknown <troyalty@vols.utk.edu>
2020-09-08 16:42:26 -0400

    forgot to remove additional convolutional layer

--- scripts/python/tmr/cnn_functions.py
+++ scripts/python/tmr/cnn_functions.py
@@ -90,11 +90,11 @@ def original_blstm(num_classes, num_letters, sequence_length, embed_size=50):
     from keras.optimizers import Adam#, Nadam
     
     model = Sequential()
-    model.add(Conv1D(input_shape=(sequence_length, num_letters), filters=320, kernel_size=26, padding="valid", activation="relu"))
+    model.add(Conv1D(input_shape=(sequence_length, num_letters), filters=100, kernel_size=26, padding="valid", activation="relu"))
     model.add(MaxPooling1D(pool_size=13, strides=13))
-    model.add(Dropout(0.1))
-    model.add(Conv1D(filters=100, kernel_size=26, padding="valid", activation="relu"))
-    model.add(MaxPooling1D(pool_size=5))
+    # model.add(Dropout(0.1))
+    # model.add(Conv1D(filters=100, kernel_size=26, padding="valid", activation="relu"))
+    # model.add(MaxPooling1D(pool_size=5))
     model.add(Masking(mask_value=0))
     model.add(Dropout(0.2))
     model.add(Bidirectional(LSTM(320, activation="tanh", return_sequences=True)))
