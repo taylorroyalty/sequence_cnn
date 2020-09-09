8f76774a70fd523ad6d2823a8d1ce39cf0cede4e
unknown <troyalty@vols.utk.edu>
2020-09-09 12:58:54 -0400

    integrating changes to cnn_function.py from base_model

--- scripts/python/tmr/cnn_functions.py
+++ scripts/python/tmr/cnn_functions.py
@@ -7,66 +7,106 @@ Created on Tue Sep  8 15:49:44 2020
 
 #%%
 
-def aa_one_hot(seqs,max_len=None):
+def seq_one_hot(seqs,seq_type='aa',max_len=None,seq_resize=True):
 # =============================================================================
-# one-hot encodes amino acid sequences. Sequences shorter than the longest sequence are 
-# paded with 0's for all amino acid features. Functionality in the future will include a nucleotide
-# option.
-# -seqs-- a list where each element is an amino acid string
+# one-hot encodes sequences for use in nn modeling.
+# seqs -- a list where each element is a biological sequence as a string 
+# seq_type -- specifies type of biological sequence. Support options are ['aa', 'dna', 'rna', 'dna_iupac']. 
+#             defualt: 'aa'
+# max_len -- specifies the length of sequences. Defualt is None. This takes the maximum length sequence as the max.
+# seq_resize -- This option resizes sequences using tensorflow.image resize  
 # =============================================================================
-    import numpy as np
-    from tensorflow.image import resize
-    #dictionary matching AA and feature index for one-hot encoded matrix
-    aa_dict= {"A": 0,
-              "C": 1,
-              "D": 2,
-              "E": 3,
-              "F": 4,
-              "G": 5,
-              "H": 6,
-              "I": 7,
-              "K": 8,
-              "L": 9,
-              "M": 10,
-              "N": 11,
-              "P": 12,
-              "Q": 13,
-              "R": 14,
-              "S": 15,
-              "T": 16,
-              "V": 17,
-              "W": 18,
-              "Y": 19,
-              "X": 20,
-              "B": 21,
-              "Z": 22,
-              "J": 23,
-              "U": 24,
-              "O": 25}
+    from numpy import zeros
+    #create dictionary matching sequences positions to feature index for one-hot encoded matrix
+    if seq_type == 'aa':
+        seq_dict= {"A": 0,
+                  "C": 1,
+                  "D": 2,
+                  "E": 3,
+                  "F": 4,
+                  "G": 5,
+                  "H": 6,
+                  "I": 7,
+                  "K": 8,
+                  "L": 9,
+                  "M": 10,
+                  "N": 11,
+                  "P": 12,
+                  "Q": 13,
+                  "R": 14,
+                  "S": 15,
+                  "T": 16,
+                  "V": 17,
+                  "W": 18,
+                  "Y": 19,
+                  "X": 20,
+                  "B": 21,
+                  "Z": 22,
+                  "J": 23,
+                  "U": 24,
+                  "O": 25}
+        n_letter=26
+    elif seq_type == 'dna':
+        seq_dict= {"A": 0,
+                  "T": 1,
+                  "C": 2,
+                  "G": 3}
+        n_letter=4
+    elif seq_type == 'dna_iupac':
+        seq_dict= {"A": 0,
+                  "T": 1,
+                  "C": 2,
+                  "G": 3,
+                  "Y": 4,
+                  "S": 5,
+                  "W": 6,
+                  "K": 7,
+                  "M": 8,
+                  "B": 9,
+                  "D": 10,
+                  "H": 11,
+                  "V": 12,
+                  "N": 13,
+                  "-": 14}
+        n_letter=15
+        
+    else:
+        return "Supported seq_type options include: ['aa', 'dna', 'rna', 'dna_iupac']"
     
     #find maximum length sequence
     if max_len == None:
         n=[len(seq) for seq in seqs]
         max_len = max(n)
     
-    #pre-define matrix based on length and number of sequences; pad 0s on end
-    #of sequences shorter than maximum length sequence
-    one_hot_matrix=np.zeros(shape=(len(seqs),max_len,26),dtype='float')    
+    #pre-define numpy matrix based on length and number of sequences
+    one_hot_matrix=zeros(shape=(len(seqs),max_len,n_letter),dtype='float')    
     
-    #indexing one_hot samples and timeseries (i.e., aa position)
+    #indexing matching bases/aa's to dictionary and populating one_hot_matrix
     #feature index is retrieved with dictionary
-    i=0
-    for seq in seqs:
-        j=0
-        tmp_vector=np.zeros(shape=(1,len(seq),26,1))
-        for aa in seq:
-            a=aa_dict[aa]
-            tmp_vector[0,j,a,0]=1
-            # one_hot_matrix[i,j,a]=1
-            j+=1
-            # if j == max_len: break
-        one_hot_matrix[i,:,:]=resize(tmp_vector,size=(max_len,26))[0,:,:,0].numpy()
-        i+=1
+    if seq_resize == True:
+        from tensorflow.image import resize
+        
+        i=0
+        for seq in seqs: #loop through each sequence in list seqs
+            j=0
+            tmp_vector=zeros(shape=(1,len(seq),n_letter,1)) #define 4-D tensor with 1 sample and 1 channel
+            for letter in seq: #loop through each base/aa in sequence
+                indx=seq_dict[letter] #match letter to dictionary
+                tmp_vector[0,j,indx,0]=1 
+                j+=1
+            one_hot_matrix[i,:,:]=resize(tmp_vector,size=(max_len,n_letter))[0,:,:,0].numpy() #reshape 4-D tensor to 2D
+            i+=1
+    else:
+        i=0
+        for seq in seqs:
+            j=0
+            for letter in seq:
+                indx=seq_dict[letter] #match letter to dictionary
+                one_hot_matrix[i,j,indx]=1
+                j+=1
+                if j == max_len: break
+            i+=1
+            
     return one_hot_matrix
 
 #%%
@@ -82,24 +122,37 @@ def load_seq_dataframe(dir_path):
         
     return seq_df
 #%%
-#model architecture
+#model architecture for amino acids
 def original_blstm(num_classes, num_letters, sequence_length, embed_size=50):
     from keras.models import Sequential
     from keras.layers import LSTM, Masking, Dense,  Bidirectional, Dropout, MaxPooling1D, Conv1D, Activation
-    # from keras.layers.normalization import BatchNormalization
     from keras.optimizers import Adam#, Nadam
     
     model = Sequential()
     model.add(Conv1D(input_shape=(sequence_length, num_letters), filters=100, kernel_size=26, padding="valid", activation="relu"))
     model.add(MaxPooling1D(pool_size=13, strides=13))
-    # model.add(Dropout(0.1))
-    # model.add(Conv1D(filters=100, kernel_size=26, padding="valid", activation="relu"))
-    # model.add(MaxPooling1D(pool_size=5))
     model.add(Masking(mask_value=0))
     model.add(Dropout(0.2))
     model.add(Bidirectional(LSTM(320, activation="tanh", return_sequences=True)))
     model.add(Dropout(0.5))
-    #model.add(LSTM(num_classes, activation="softmax", name="AV"))
+    model.add(LSTM(embed_size, activation="tanh"))
+    model.add(Dense(num_classes, activation=None, name="AV"))
+    model.add(Activation("softmax"))
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
+    return model
+#%%
+def dna_blstm(num_classes, num_letters, sequence_length, embed_size=256):
+    from keras.models import Sequential
+    from keras.layers import LSTM, Dense,  Bidirectional, Dropout, MaxPooling1D, Conv1D, Activation
+    from keras.optimizers import Adam#, Nadam
+    
+    model = Sequential()
+    model.add(Conv1D(input_shape=(sequence_length, num_letters), filters=26, kernel_size=3, strides=3, padding="valid", activation="relu"))
+    model.add(Conv1D(filters=320, kernel_size=26, padding="valid", activation="relu"))
+    model.add(MaxPooling1D(pool_length=13, stride=13))
+    model.add(Dropout(0.2))
+    model.add(Bidirectional(LSTM(320, activation="tanh", return_sequences=True)))
+    model.add(Dropout(0.5))
     model.add(LSTM(embed_size, activation="tanh"))
     model.add(Dense(num_classes, activation=None, name="AV"))
     model.add(Activation("softmax"))
@@ -109,7 +162,7 @@ def original_blstm(num_classes, num_letters, sequence_length, embed_size=50):
 #%%
 
 def tsne_non_trained_classes(model,data,write_path,layer,max_len):
-        import pandas as pd
+        # import pandas as pd
         from keras.models import Model
         from sklearn.manifold import TSNE
         # import matplotlib.pyplot as plt
@@ -117,7 +170,7 @@ def tsne_non_trained_classes(model,data,write_path,layer,max_len):
         embed_model = Model(inputs=model.input, outputs=model.get_layer(layer).output)
         embed_model.summary()
         
-        new_seq=aa_one_hot(data['sequence'],max_len=max_len)
+        new_seq=seq_one_hot(data['sequence'],max_len=max_len)
         embed = embed_model.predict(new_seq, batch_size=100, verbose=1)
         tsne = TSNE(n_components=2, random_state=0)
         xx = tsne.fit_transform(embed)
--- scripts/python/tmr/cnn_script.py
+++ scripts/python/tmr/cnn_script.py
@@ -7,9 +7,7 @@ Created on Mon Aug 31 07:54:42 2020
 #%%
 # Libraries and modules
 import pandas as pd
-# import random
 import numpy as np
-# from model_templates_tmr import original_blstm
 from keras.utils import to_categorical
 
 import sys
@@ -18,25 +16,26 @@ sys.path.insert(1,'scripts/python/tmr/')
 import cnn_functions as cf
 #%%
 #Inputs
-layer="lstm_3"
+layer="lstm_1"
 tnse_write_path='data/tnse_results/'
 emb_data_path='data/swiss_1_99.tsv'
-cluster_dataframe_path='data/cluster_dataframes/'
+data_path='data/cluster_dataframes/'
 model_save_path='data/models/'
 
-magn=20
-max_len=100
-sample_frac=0.2
-is_dna_data=False
-mask_length = None
+#nn parameters
+max_len=1500
+sample_n=3
 embed_size = 256
 batch_size=100
-epochs=50
-
+epochs=5
+cnn_fun_path='scripts/python/tmr/'
+seq_type='aa'
+num_letters=26
+seq_resize=True 
 
 #%%
 #generate datasets for fitting
-seq_df=cf.load_seq_dataframe(cluster_dataframe_path)
+seq_df=cf.load_seq_dataframe(data_path)
 uniq_anno=seq_df.annotation.unique()
 num_classes=len(uniq_anno)
 annotation_ydata_df=pd.DataFrame({'ydata': range(num_classes),'annotation': uniq_anno})
@@ -48,42 +47,57 @@ seq_cluster_a=seq_cluster
 #%%
 #generate training data for annotation/cluster datasets
 ##annotations
-# train_a=seq_df.groupby(['annotation']).sample(frac=sample_frac)
-# seq_df=seq_df.drop(train_a.index)
-# train_a_one_hot=aa_one_hot(train_a['sequence'],max_len=max_len)
-train_a=seq_cluster_a.groupby(['annotation']).sample(frac=sample_frac)
+train_a=seq_cluster_a.groupby(['annotation']).sample(n=sample_n)
 seq_cluster_a=seq_cluster_a.drop(train_a.index)
-train_a_one_hot=cf.aa_one_hot(train_a['sequence'],max_len=max_len)
+train_a_one_hot=cf.seq_one_hot(train_a['sequence'],
+                              seq_type=seq_type,
+                              max_len=max_len,
+                              seq_resize=seq_resize)
 
 ##clusters
-train_c=seq_cluster.groupby(['annotation','Cluster']).sample(frac=sample_frac)
+train_c=seq_cluster.groupby(['annotation','Cluster']).sample(n=sample_n)
 seq_cluster=seq_cluster.drop(train_c.index)
-train_c_one_hot=cf.aa_one_hot(train_c['sequence'],max_len=max_len)
+train_c_one_hot=cf.seq_one_hot(train_c['sequence'],
+                              seq_type=seq_type,
+                              max_len=max_len,
+                              seq_resize=seq_resize)
 
 #%%
 #generate validation data for annotation/cluster datasets
 ##annotation
-# validation_a=seq_df.groupby(['annotation']).sample(frac=sample_frac)
-# seq_df=seq_df.drop(validation_a.index)
-# validation_a_one_hot=aa_one_hot(validation_a['sequence'],max_len=max_len)
-validation_a=seq_cluster_a.groupby(['annotation']).sample(frac=sample_frac)
+validation_a=seq_cluster_a.groupby(['annotation']).sample(n=sample_n)
 seq_cluster_a=seq_cluster_a.drop(validation_a.index)
-validation_a_one_hot=cf.aa_one_hot(validation_a['sequence'],max_len=max_len)
+validation_a_one_hot=cf.seq_one_hot(validation_a['sequence'],
+                              seq_type=seq_type,
+                              max_len=max_len,
+                              seq_resize=seq_resize)
 
 ##clusters
-validation_c=seq_cluster.groupby(['annotation','Cluster']).sample(frac=sample_frac)
+validation_c=seq_cluster.groupby(['annotation','Cluster']).sample(n=sample_n)
 seq_cluster=seq_cluster.drop(validation_c.index)
-validation_c_one_hot=cf.aa_one_hot(validation_c['sequence'],max_len=max_len)
+validation_c_one_hot=cf.seq_one_hot(validation_c['sequence'],
+                              seq_type=seq_type,
+                              max_len=max_len,
+                              seq_resize=seq_resize)
 
 #generate test data for annotation/cluster datasets
 ##annotation
 
 test_a=seq_cluster_a
-test_a_one_hot=cf.aa_one_hot(test_a['sequence'],max_len=max_len)
+test_a_one_hot=cf.seq_one_hot(test_a['sequence'],
+                              seq_type=seq_type,
+                              max_len=max_len,
+                              seq_resize=seq_resize)
 #clusters
 test_c=seq_cluster
-test_c_one_hot=cf.aa_one_hot(test_c['sequence'],max_len=max_len)
-test_noise_one_hot=cf.aa_one_hot(seq_cluster_noise['sequence'],max_len=max_len)
+test_c_one_hot=cf.seq_one_hot(test_c['sequence'],
+                              seq_type=seq_type,
+                              max_len=max_len,
+                              seq_resize=seq_resize)
+test_noise_one_hot=cf.seq_one_hot(seq_cluster_noise['sequence'],
+                              seq_type=seq_type,
+                              max_len=max_len,
+                              seq_resize=seq_resize)
 
 #%%
 ##generate y data for annotation/cluster datasets
@@ -97,10 +111,16 @@ yvalidation_c=to_categorical(np.array(validation_c.ydata,dtype='uint8'),num_clas
 ytest_c=to_categorical(np.array(test_c.ydata,dtype='uint8'),num_classes)
 ytest_noise=to_categorical(np.array(seq_cluster_noise.ydata,dtype='uint8'),num_classes)
 
-num_letters = 4 if is_dna_data else 26
+
 # sequence_length = train_one_hot.shape[1]
-model_a= cf.original_blstm(num_classes, num_letters, max_len, embed_size=embed_size)
-model_c= cf.original_blstm(num_classes, num_letters, max_len, embed_size=embed_size)
+model_a= cf.original_blstm(num_classes,
+                           num_letters,
+                           max_len,
+                           embed_size=embed_size)
+model_c= cf.original_blstm(num_classes,
+                           num_letters,
+                           max_len,
+                           embed_size=embed_size)
 
 # n_train_a=train_a_one_hot.shape[0]
 # n_train_c=train_c_one_hot.shape[0]
@@ -109,10 +129,10 @@ n_validation_a=validation_a_one_hot.shape[0]
 n_validation_c=validation_c_one_hot.shape[0]
 
 model_a.fit(x=train_a_one_hot,y=ytrain_a,batch_size=batch_size,
-            validation_data=(validation_a_one_hot,yvalidation_a),validation_batch_size=magn*batch_size,
+            validation_data=(validation_a_one_hot,yvalidation_a),
             epochs=epochs)
 model_c.fit(x=train_c_one_hot,y=ytrain_c,batch_size=batch_size,
-            validation_data=(validation_c_one_hot,yvalidation_c),validation_batch_size=magn*batch_size,
+            validation_data=(validation_c_one_hot,yvalidation_c),
             epochs=epochs)
 
 
@@ -126,73 +146,5 @@ model_c.evaluate(test_noise_one_hot,ytest_noise)
 
 emb_data=pd.read_csv(emb_data_path,sep='\t').groupby("annotation").filter(lambda x: len(x)>9).reset_index(drop=True)
 cf.tsne_non_trained_classes(model_a,emb_data,tnse_write_path,layer,max_len)
-
-# for i in range(epochs):
-#     #generate indices for building training batch datasets
-#     sample_train_a_index=random.sample(range(n_train_a),batch_size)
-#     sample_train_c_index=random.sample(range(n_train_c),batch_size)
-    
-#     #generate indices for building validation batch datasets
-#     sample_validation_a_index=random.sample(range(n_validation_a),batch_size)
-#     sample_validation_c_index=random.sample(range(n_validation_c),batch_size)
-    
-#     #batch datasets
-#     ##training
-#     tmp_train_a_x=train_a_one_hot[sample_train_a_index,:,:]
-#     tmp_train_a_y=ytrain_a[sample_train_a_index,:]
-#     tmp_train_c_x=train_c_one_hot[sample_train_c_index,:,:]
-#     tmp_train_c_y=ytrain_c[sample_train_c_index,:]
-    
-#     ##validation
-#     tmp_validation_a_x=validation_a_one_hot[sample_validation_a_index,:,:]
-#     tmp_validation_a_y=yvalidation_a[sample_validation_a_index,:]
-#     tmp_validation_c_x=validation_c_one_hot[sample_validation_c_index,:,:]
-#     tmp_validation_c_y=yvalidation_c[sample_validation_c_index,:]
-    
-#     #fit models with batch datasets
-#     model_a.train_on_batch(tmp_train_a_x,tmp_train_a_y,validation_data=(tmp_validation_a_x,tmp_validation_a_y))
-#     model_c.train_on_batch(tmp_train_c_x,tmp_train_c_y,validation_data=(tmp_validation_c_x,tmp_validation_c_y)) 
-
-
-
-
-# model.evaluate(test_one_hot,ytest)
-# model.evaluate(test_noise_one_hot,ytest_noise)
-
-##fit model
-
-# model.fit(x=train_one_hot,y=ytrain,batch_size=100,epochs=500,validation_data=(validation_one_hot,yvalidation))
-# =============================================================================
-# model.evaluate(test_one_hot,ytest)
-# 
-# 
-# 
-# #cluster sampling
-# train=seq_cluster.groupby(['annotation','Cluster']).sample(5)
-# seq_cluster=seq_cluster.drop(train.index)
-# validation=seq_cluster.groupby(['annotation','Cluster']).sample(5)
-# seq_cluster=seq_cluster.drop(validation.index)
-# test=seq_cluster
-# 
-# 
-# 
-# 
-# 
-# num_letters = 4 if is_dna_data else 26
-# sequence_length = train_one_hot.shape[1]
-# model = original_blstm(num_classes, num_letters, max_len, embed_size=embed_size)
-# 
-# ##fit model
-# ytrain=to_categorical(np.array(train.ydata,dtype='uint8'),num_classes)
-# yvalidation=to_categorical(np.array(validation.ydata,dtype='uint8'),num_classes)
-# ytest=to_categorical(np.array(test.ydata,dtype='uint8'),num_classes)
-# ytest_noise=to_categorical(np.array(seq_cluster_noise.ydata,dtype='uint8'),num_classes)
-# =============================================================================
-
-# # model.fit(x=train_one_hot,y=ytrain,batch_size=100,epochs=500,validation_data=(validation_one_hot,yvalidation))
-# model.evaluate(test_one_hot,ytest)
-# model.evaluate(test_noise_one_hot,ytest_noise)
-
-# model.save(model_save_path + 'swiss100_annotation_clusters.h5')
 
     
